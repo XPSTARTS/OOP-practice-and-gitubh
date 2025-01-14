35889f8c4dd4d36593ce4b97dacfe7a04523e9a9
XPSTARTS <abdulmoid47628@gmail.com>
2025-01-14 23:49:40 +0500

    Shop Management program to leran OOP

--- 1st.cpp
+++ 1st.cpp
@@ -2,61 +2,63 @@
 #include <cmath>
 #include <string>
 using namespace std;
-   
-     class binarycheck{
-     private:
-     string s;
-     public:
-      
-      void setdata();
-      void getdata();
-      void ones_compliment();
-  
 
-     };
-     void binarycheck :: setdata()
-     {
-      cout<<"Enter the binary number: ";
-      cin>>s;
-     }
-     void binarycheck ::  getdata()
-     {
-        for(int i=0; i<s.length();i++)
-        {
-         if(s.at(i)!='0' && s.at(i)!='1')
-         {
-            cout<<"Enter number is not binary!"<<endl;
-            return ;
-         }    
-        }
-         cout<<"Enter number is binary!"<<endl;
-     }
+  // Making a program for a shop
+  /*  
+  It consist of an Item Number that is an array of size 100
+  Item Price that is also an array of size 100
+  Date of Manufacture and expiry
+  Working: This program asks the user to enter the data and then displays it.
+  */
 
-     void binarycheck ::  ones_compliment()
-     {
-      bool flag=true;
-        for(int i=0; i<s.length();i++)
-        {
-         s.at(i) = (s.at(i) == '0') ? '1' : '0';
-        }
-        if(flag)
-        {
-         cout<<"One's Compliment of the binary number is: ";
-          for(int i=0; i<s.length();i++)
-          {
-            cout<<s.at(i);
-          }
-        }
-     }
+ class shop{
 
-  
+   public:
+   int itemNo[100],itemprice[100];
+   string manufactdate[100],expirydate[100];
+   int counter=0;
+
+   void setdata();
+   void getdata();
+
+ };
+
+  void shop :: setdata()
+  {
+   cout<<"Enter the item number of Product "<<counter+1<<" :";
+   cin>>itemNo[counter];
+   cout<<"Enter the price of Product "<<counter+1<<" :";
+   cin>>itemprice[counter];
+   cin.ignore();
+   cout<<"Enter the date of manufacutring: ";
+   getline(cin,manufactdate[counter]);
+   cout<<"Enter the date of Expiry: ";
+   getline(cin,expirydate[counter]);
+   counter++;
+  }
+  void shop:: getdata()
+  {
+   for(int i=0;i<counter;i++)
+   {
+      cout<<endl<<endl;
+      cout<<"Item Number of Product "<<i<<" is: "<<itemNo[i]<<endl;
+      cout<<"price of Product "<<i<<" is: "<<itemprice[i]<<" $"<<endl;
+      cout<<"Date of manufacture is: "<<manufactdate[i]<<endl;
+      cout<<"Date of expiry is: "<<expirydate[i]<<endl;
+   }
+  }
 
 int main(){
-   binarycheck b;
-   b.setdata();
-   b.getdata();
-   b.ones_compliment();
-    
-    
+   cout<<"\t*********************************Welcome to the shop management*********************************"<<endl;
+   shop s;
+   int n;
+   cout<<"Enter the number of products: ";
+   cin>>n;
+   for(int i=0;i<n;i++)
+   {
+   s.setdata();
+   }
+   s.getdata();
+  
 return 0;
 }
