b94e6581d7a7b6457c528d4af6a220d9d41e75cb
XPSTARTS <abdulmoid47628@gmail.com>
2025-01-15 00:37:12 +0500

    Program demonstrating static variable and functions

--- 1st.cpp
+++ 1st.cpp
@@ -2,63 +2,47 @@
 #include <cmath>
 #include <string>
 using namespace std;
-
-  // Making a program for a shop
-  /*  
-  It consist of an Item Number that is an array of size 100
-  Item Price that is also an array of size 100
-  Date of Manufacture and expiry
-  Working: This program asks the user to enter the data and then displays it.
+  // Program for static variable and functions
+  /*Static vatiable by default value is 0 (it start from zero).
+    static variable has a syantax as shown in the program.
+    Its a variable that is declared in Dynamic memory means it cannot be vary from function to function.
   */
+ /*  Static functions displays or workwith static function.
+     Same uses as static variable.
+ */
 
- class shop{
+  class student{
+    public:
+    static int rollNo;
+    void setdata()
+    {
+      
+      cout<<"FIrst Student name is Abdul Moid and their roll number is: "<<rollNo<<endl;
+      cout<<"FIrst Student name is Abdul Momin and their roll number is: "<<rollNo+2<<endl;
+      cout<<"FIrst Student name is Abdullah dilawar and their roll number is: "<<rollNo+4<<endl;
+    }
+  };
+   int student::rollNo=1;
 
-   public:
-   int itemNo[100],itemprice[100];
-   string manufactdate[100],expirydate[100];
-   int counter=0;
+   class employee{
+    public:
+    static int employeeid;
+    
+   static void setdata(void)
+    {
+        cout<<"Employee Shoaib Ahmed has an id "<<employeeid<<endl;
+        cout<<"Employee Muneeb Ahmed has an id "<<employeeid+3<<endl;
+        cout<<"Employee Ijaz Ahmed has an id "<<employeeid+100<<endl;
+    }
 
-   void setdata();
-   void getdata();
+   };
 
- };
-
-  void shop :: setdata()
-  {
-   cout<<"Enter the item number of Product "<<counter+1<<" :";
-   cin>>itemNo[counter];
-   cout<<"Enter the price of Product "<<counter+1<<" :";
-   cin>>itemprice[counter];
-   cin.ignore();
-   cout<<"Enter the date of manufacutring: ";
-   getline(cin,manufactdate[counter]);
-   cout<<"Enter the date of Expiry: ";
-   getline(cin,expirydate[counter]);
-   counter++;
-  }
-  void shop:: getdata()
-  {
-   for(int i=0;i<counter;i++)
-   {
-      cout<<endl<<endl;
-      cout<<"Item Number of Product "<<i<<" is: "<<itemNo[i]<<endl;
-      cout<<"price of Product "<<i<<" is: "<<itemprice[i]<<" $"<<endl;
-      cout<<"Date of manufacture is: "<<manufactdate[i]<<endl;
-      cout<<"Date of expiry is: "<<expirydate[i]<<endl;
-   }
-  }
+   int employee :: employeeid=65;
 
 int main(){
-   cout<<"\t*********************************Welcome to the shop management*********************************"<<endl;
-   shop s;
-   int n;
-   cout<<"Enter the number of products: ";
-   cin>>n;
-   for(int i=0;i<n;i++)
-   {
+   student s;
    s.setdata();
-   }
-   s.getdata();
-  
+   employee e;
+   e.setdata();
 return 0;
 }
