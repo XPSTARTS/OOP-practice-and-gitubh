aa829a32966c54c8eac08a22f8313c91ad6e824a
XPSTARTS <abdulmoid47628@gmail.com>
2025-01-14 21:39:41 +0500

    Check binary program to demonstrate nesting in OOP

--- 1st.cpp
+++ 1st.cpp
@@ -3,50 +3,59 @@
 #include <string>
 using namespace std;
    
-    // CLass Demonstrating the use of Access Modifiers in C++
-   class students{
+     class binarycheck{
      private:
-     string PhoneNo; //Declaring Private variables
-     string address,GfName;   
-
+     string s;
      public:
-     string RollNo;
-     string name,year;
+      
+      void setdata();
+      void getdata();
+      void ones_compliment();
+  
 
-     void setdata()  //We have to initialize all the private members here because we cannot do this in main or outside this function.
+     };
+     void binarycheck :: setdata()
      {
-        
-     cout<<"Enter your Phone Number: ";
-     cin>>PhoneNo;
-     cin.ignore();
-    cout<<"Enter your address: ";
-    getline(cin,address);
-    cout<<"Enter your GfName: ";
-    getline(cin,GfName);
+      cout<<"Enter the binary number: ";
+      cin>>s;
      }
-     void getdata(){
-        cout<<"Name of the student is: "<<name<<endl;
-        cout<<"Student is in the Class: "<<year<<endl;
-        cout<<"Enrollment of the student is: "<<RollNo<<endl;
-        cout<<"Address of the student is: "<<address<<endl;
-        cout<<"Name of his Girlfriend is: "<<GfName<<endl;
-        cout<<"His Contact Number is: "<<PhoneNo<<endl;
+     void binarycheck ::  getdata()
+     {
+        for(int i=0; i<s.length();i++)
+        {
+         if(s.at(i)!='0' && s.at(i)!='1')
+         {
+            cout<<"Enter number is not binary!"<<endl;
+            return ;
+         }    
+        }
+         cout<<"Enter number is binary!"<<endl;
      }
-   };
+
+     void binarycheck ::  ones_compliment()
+     {
+      bool flag=true;
+        for(int i=0; i<s.length();i++)
+        {
+         s.at(i) = (s.at(i) == '0') ? '1' : '0';
+        }
+        if(flag)
+        {
+         cout<<"One's Compliment of the binary number is: ";
+          for(int i=0; i<s.length();i++)
+          {
+            cout<<s.at(i);
+          }
+        }
+     }
 
   
 
 int main(){
-     students s1;
-    cout<<"Enter your name: ";
-    getline(cin,s1.name);
-     cout<<"Enter your Class/Section: ";
-    getline(cin,s1.year);
-     cout<<"Enter your Enrollment Number: ";
-     cin>> s1.RollNo; 
-     cin.ignore();
-s1.setdata();
-s1.getdata();
+   binarycheck b;
+   b.setdata();
+   b.getdata();
+   b.ones_compliment();
     
     
 return 0;
