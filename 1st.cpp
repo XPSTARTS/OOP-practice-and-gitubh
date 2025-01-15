667b4889df60e5e406558bde4db0a9bd5a15f290
XPSTARTS <abdulmoid47628@gmail.com>
2025-01-15 22:28:27 +0500

    A simple program demonstrating arrays in an object

--- 1st.cpp
+++ 1st.cpp
@@ -2,47 +2,35 @@
 #include <cmath>
 #include <string>
 using namespace std;
-  // Program for static variable and functions
-  /*Static vatiable by default value is 0 (it start from zero).
-    static variable has a syantax as shown in the program.
-    Its a variable that is declared in Dynamic memory means it cannot be vary from function to function.
-  */
- /*  Static functions displays or workwith static function.
-     Same uses as static variable.
- */
-
-  class student{
-    public:
-    static int rollNo;
-    void setdata()
-    {
-      
-      cout<<"FIrst Student name is Abdul Moid and their roll number is: "<<rollNo<<endl;
-      cout<<"FIrst Student name is Abdul Momin and their roll number is: "<<rollNo+2<<endl;
-      cout<<"FIrst Student name is Abdullah dilawar and their roll number is: "<<rollNo+4<<endl;
-    }
-  };
-   int student::rollNo=1;
-
+   
    class employee{
-    public:
-    static int employeeid;
     
-   static void setdata(void)
-    {
-        cout<<"Employee Shoaib Ahmed has an id "<<employeeid<<endl;
-        cout<<"Employee Muneeb Ahmed has an id "<<employeeid+3<<endl;
-        cout<<"Employee Ijaz Ahmed has an id "<<employeeid+100<<endl;
+    int id,salary;
+    public:
+    static int counter;
+    void setdata(){
+      cout<<"Enter employee id : ", cin>>id;
+      cout<<"Enter salary of employee with id \" "<<id<<" \" :", cin>>salary;
     }
-
+    void getdata()
+    {
+      cout<<"Employee with id: "<<id<<" have a salary: "<<salary<<" $"<<endl;  
+    }
    };
 
-   int employee :: employeeid=65;
+
 
 int main(){
-   student s;
-   s.setdata();
-   employee e;
-   e.setdata();
+   employee amazon[3];
+   for(int i=0;i<3;i++)
+   {
+    amazon[i].setdata();
+   }
+   cout<<endl;
+   cout<<endl;
+   for(int i=0;i<3;i++)
+   {
+   amazon[i].getdata();
+   }
 return 0;
 }
