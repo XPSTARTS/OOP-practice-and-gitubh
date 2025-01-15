c19bc84ed83c0dc35a1309d7f5d8352db59ff7c2
XPSTARTS <abdulmoid47628@gmail.com>
2025-01-15 22:43:48 +0500

    This simple program demonstrating the passing object in function as parameter

--- 1st.cpp
+++ 1st.cpp
@@ -2,35 +2,45 @@
 #include <cmath>
 #include <string>
 using namespace std;
-   
-   class employee{
-    
-    int id,salary;
-    public:
-    static int counter;
-    void setdata(){
-      cout<<"Enter employee id : ", cin>>id;
-      cout<<"Enter salary of employee with id \" "<<id<<" \" :", cin>>salary;
-    }
-    void getdata()
-    {
-      cout<<"Employee with id: "<<id<<" have a salary: "<<salary<<" $"<<endl;  
-    }
+  
+  /*
+  Making an program that print complex number and helps to learn about passing object as arguments.
+  */
+   class complex{
+     int a , b;
+     public:
+     void setdata(int v1 , int v2)
+     {
+       a=v1;
+       b=v2;
+       
+     }
+
+     void getdata(complex r, complex i)  // where printcomplex is class name and we're passing objects in class functions as arguments
+     {
+         a=r.a+i.a;
+         b=r.b=i.b;
+     }
+
+     void print()
+     {
+      cout<<"The complex number is: "<<a<<" + "<<b<<"i"<<endl;
+     }
+
    };
 
 
+int main(){ 
+  complex c1,c2,c3;
 
-int main(){
-   employee amazon[3];
-   for(int i=0;i<3;i++)
-   {
-    amazon[i].setdata();
-   }
-   cout<<endl;
-   cout<<endl;
-   for(int i=0;i<3;i++)
-   {
-   amazon[i].getdata();
-   }
+   c1.setdata(1,2);
+   c1.print();
+
+   c2.setdata(3,8);
+   c2.print();
+
+   c3.setdata(10,22);
+   c3.print();
+
 return 0;
 }
