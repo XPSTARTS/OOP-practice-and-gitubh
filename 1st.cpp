ce303db838c2a86a5802bae52ba6e698c92ba7ba
XPSTARTS <abdulmoid47628@gmail.com>
2025-01-19 01:01:42 +0500

    a swapping variable program showing friend function demostration

--- 1st.cpp
+++ 1st.cpp
@@ -5,41 +5,55 @@ using namespace std;
 
   class Y;
 
-   class X{
+  class X{
     int a;
+    friend  void swapnum(X & , Y &);
     public:
-    
-    friend void prodof( X ,Y);
-    void seta(int value){
+    void setvalue(int value)
+    {
       a=value;
     }
-   };
+    void display()
+    {
+      cout<<"The first value before swapping is: "<<a<<endl;
+    }
+  };
 
-   class Y{
+  class Y{
     int b;
+    friend  void swapnum(X & , Y &);
     public:
-
-    friend void prodof(X , Y);
-
-    void setb(int value) {
+    void setvalue(int value)
+    {
       b=value;
     }
+    void display()
+    {
+      cout<<"The second value before swapping is: "<<b<<endl;
+    }
+  };
 
-   };
-
-   void prodof(X p1 , Y p2)
-   {
-    int prod = p1.a * p2.b;
-    cout<<"The Product of the numbers are: "<<prod<<endl;
-   }
+  void swapnum(X &a , Y &b)
+  {
+    int temp=a.a;
+    a.a=b.b;
+    b.b=temp;
+  }
 
 int main(){
+   X num1;
+   num1.setvalue(10);
+   num1.display();
 
-  X num1;
-  num1.seta(3);
-  Y num2;
-  num2.setb(200);
-  prodof(num1 , num2 );
+   Y num2;
+   num2.setvalue(100);
+   num2.display();
+
+   swapnum(num1 , num2);
+   cout<<"After swapping: "<<endl;
+   num1.display();
+   num2.display();
+ 
 
 return 0;
 }
